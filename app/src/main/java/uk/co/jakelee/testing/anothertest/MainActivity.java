7ead0ee2153be4b80a0ef3d4bb89d6f799da8cc6
Jake <jake_l@live.co.uk>
2015-11-04 07:30:20 +0000

    Tutorial 1 complete.

---
+++ app/src/main/java/uk/co/jakelee/testing/anothertest/MainActivity.java
@@ -0,0 +1,25 @@
+package uk.co.jakelee.testing.anothertest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "uk.co.jakelee.testing.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
