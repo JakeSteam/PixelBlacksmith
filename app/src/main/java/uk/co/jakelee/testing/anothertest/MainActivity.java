809632cf1df592134d278cea9d1656733cfd0823
Jake <jake_l@live.co.uk>
2015-11-05 03:55:24 +0000

    Saving and loading variables.

--- app/src/main/java/uk/co/jakelee/testing/anothertest/MainActivity.java
+++ app/src/main/java/uk/co/jakelee/testing/anothertest/MainActivity.java
@@ -1,17 +1,29 @@
 package uk.co.jakelee.testing.anothertest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "uk.co.jakelee.testing.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setIntSetting("copperOreCount", 1);
+        setIntSetting("tinOreCount", 2);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView copperOreCount = (TextView) findViewById(R.id.copperOreCountLabel);
+        copperOreCount.setText(Integer.toString(getIntSetting("copperOreCount", 0)));
+
+        TextView tinOreCount = (TextView) findViewById(R.id.tinOreCountLabel);
+        tinOreCount.setText(Integer.toString(getIntSetting("tinOreCount", 0)));
     }
 
     public void openFurnace(View view) {
@@ -26,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
         String message = "This is an anvil";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public int getIntSetting(String variableName, int defaultValue) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        return sharedPref.getInt(variableName, defaultValue);
+    }
+
+    public void setIntSetting(String variableName, int value) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(variableName, value);
+        editor.commit();
     }
 }
