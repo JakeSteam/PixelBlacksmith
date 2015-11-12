edd3e91a9cc3ffeff23cc2fd9ea23ae9114f0e5b
Jake <jake_l@live.co.uk>
2015-11-12 07:47:36 +0000

    Popup implemented.

--- app/src/main/java/uk/co/jakelee/blacksmith/main/AnvilActivity.java
+++ app/src/main/java/uk/co/jakelee/blacksmith/main/AnvilActivity.java
@@ -1,13 +1,57 @@
 package uk.co.jakelee.blacksmith.main;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
-public class AnvilActivity extends AppCompatActivity {
+public class AnvilActivity extends Activity {
 
-    @Override
+    PopupWindow popUp;
+    LinearLayout layout;
+    TextView tv;
+    LayoutParams params;
+    LinearLayout mainLayout;
+    Button but;
+    boolean click = true;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anvil);
+        popUp = new PopupWindow(this);
+        layout = new LinearLayout(this);
+        mainLayout = new LinearLayout(this);
+        tv = new TextView(this);
+        but = new Button(this);
+        but.setText("Click Me");
+        but.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                if (click) {
+                    popUp.showAtLocation(mainLayout, Gravity.TOP, 150, 150);
+                    popUp.update(10, 10, 600, 1200);
+                    click = false;
+                } else {
+                    popUp.dismiss();
+                    click = true;
+                }
+            }
+
+        });
+        params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        tv.setText("Hi this is a sample text for popup window");
+        layout.addView(tv, params);
+        popUp.setContentView(layout);
+        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+        mainLayout.addView(but, params);
+        setContentView(mainLayout);
     }
 }
--- app/src/main/java/uk/co/jakelee/blacksmith/main/MainActivity.java
+++ app/src/main/java/uk/co/jakelee/blacksmith/main/MainActivity.java
@@ -1,47 +1,66 @@
 package uk.co.jakelee.blacksmith.main;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
 
 import uk.co.jakelee.blacksmith.sqlite.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
     public static DatabaseHelper dbh;
+    Button btnClosePopup;
+    ImageButton btnCreatePopup;
+    private PopupWindow pwindo;
+    private View.OnClickListener cancel_button_click_listener = new View.OnClickListener() {
+        public void onClick(View v) {
+            pwindo.dismiss();
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbh = new DatabaseHelper(getApplicationContext());
+        btnCreatePopup = (ImageButton) findViewById(R.id.open_furnace);
+        btnCreatePopup.setOnClickListener(new View.OnClickListener() {
 
-        updateInterface();
+            @Override
+            public void onClick(View v) {
+                initiatePopupWindow();
+            }
+        });
+    }
+
+    private void initiatePopupWindow() {
+        try {
+            LayoutInflater inflater = (LayoutInflater) MainActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.activity_furnace,
+                    (ViewGroup) findViewById(R.id.furnace));
+            pwindo = new PopupWindow(layout, 800, 1400, true);
+            pwindo.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+            btnClosePopup = (Button) layout.findViewById(R.id.btn_close_popup);
+            btnClosePopup.setOnClickListener(cancel_button_click_listener);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateInterface();
-    }
-
-    public void addCopperOre(View view) {
-        dbh.increaseInventoryQuantity(1, 1);
-
-        updateInterface();
-    }
-
-    public void addTinOre(View view) {
-        dbh.increaseInventoryQuantity(2, 1);
-
-        updateInterface();
-    }
-
-    public void addBronzeBar(View view) {
-        dbh.increaseInventoryQuantity(11, 1);
-
-        updateInterface();
     }
 
     public void openFurnace(View view) {
@@ -52,17 +71,6 @@ public class MainActivity extends AppCompatActivity {
     public void openAnvil(View view) {
         Intent intent = new Intent(this, AnvilActivity.class);
         startActivity(intent);
-    }
-
-    public void updateInterface() {
-        TextView copperOreCount = (TextView) findViewById(R.id.copperOreCountLabel);
-        copperOreCount.setText(Integer.toString(dbh.getInventoryByItem(1).getQuantity()));
-
-        TextView tinOreCount = (TextView) findViewById(R.id.tinOreCountLabel);
-        tinOreCount.setText(Integer.toString(dbh.getInventoryByItem(2).getQuantity()));
-
-        TextView bronzeBarCount = (TextView) findViewById(R.id.bronzeBarCountLabel);
-        bronzeBarCount.setText(Integer.toString(dbh.getInventoryByItem(11).getQuantity()));
     }
 
 
