821117d8a69d6d711dfc9a48db1b3600b00f47ce
Jake <jake_l@live.co.uk>
2016-03-20 19:23:37 +0000

    Added basic Google login.

--- Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/MainActivity.java
+++ Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/MainActivity.java
@@ -11,6 +11,12 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
 import uk.co.jakelee.blacksmith.R;
 import uk.co.jakelee.blacksmith.controls.TextViewPixel;
 import uk.co.jakelee.blacksmith.helper.Constants;
@@ -27,7 +33,9 @@ import uk.co.jakelee.blacksmith.model.Upgrade;
 import uk.co.jakelee.blacksmith.model.Visitor;
 import uk.co.jakelee.blacksmith.service.MusicService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
     public static DisplayHelper dh;
     public static Handler handler = new Handler();
     public int newVisitors;
@@ -49,6 +57,12 @@ public class MainActivity extends AppCompatActivity {
     public static RelativeLayout marketSlots;
     public static RelativeLayout tableSlots;
     public static RelativeLayout enchantingSlots;
+
+    private GoogleApiClient mGoogleApiClient;
+    private static int RC_SIGN_IN = 9001;
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInFlow = true;
+    private boolean mSignInClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +91,13 @@ public class MainActivity extends AppCompatActivity {
             DatabaseHelper.initialSQL();
         }
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .addApi(Drive.API).addScope(Drive.SCOPE_APPFOLDER)
+                .build();
+
         createSlots();
     }
 
@@ -95,6 +116,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -143,6 +166,8 @@ public class MainActivity extends AppCompatActivity {
             stopService(musicService);
             musicServiceIsStarted = false;
         }
+
+        mGoogleApiClient.disconnect();
     }
 
     private void setupRecurringEvents() {
@@ -266,5 +291,75 @@ public class MainActivity extends AppCompatActivity {
     public void openUpgrades(View view) {
         Intent intent = new Intent(this, UpgradeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // The player is signed in. Hide the sign-in button and allow the
+        // player to proceed.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure) {
+            // already resolving
+            return;
+        }
+
+        // if the sign-in button was clicked or if auto sign-in is enabled,
+        // launch the sign-in flow
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+
+            // Attempt to resolve the connection failure using BaseGameUtils.
+            // The R.string.signin_other_error value should reference a generic
+            // error string in your strings.xml file, such as "There was
+            // an issue with sign-in, please try again later."
+            if (!BaseGameUtils.resolveConnectionFailure(this,
+                    mGoogleApiClient, connectionResult,
+                    RC_SIGN_IN, "Sign in error")) {
+                mResolvingConnectionFailure = false;
+            }
+        }
+
+        // Put code here to display the sign-in button
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Attempt to reconnect
+        mGoogleApiClient.connect();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            } else {
+                // Bring up an error dialog to alert the user that sign-in
+                // failed. The R.string.signin_failure should reference an error
+                // string in your strings.xml file that tells the user they
+                // could not be signed in, such as "Unable to sign in."
+                BaseGameUtils.showActivityResultError(this,
+                        requestCode, resultCode, R.string.unknown_error);
+            }
+        }
+    }
+
+    // Call when the sign-in button is clicked
+    public void signInClicked(View v) {
+        mSignInClicked = true;
+        mGoogleApiClient.connect();
+    }
+
+    // Call when the sign-out button is clicked
+    private void signOutClicked() {
+        mSignInClicked = false;
+        Games.signOut(mGoogleApiClient);
     }
 }
