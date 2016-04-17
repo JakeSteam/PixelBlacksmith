191370b5d69da375b7628f141f2122bf5cee9f25
Jake <jake_l@live.co.uk>
2016-04-17 22:15:30 +0100

    Well, fully implemented IAPs.

--- Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/PremiumActivity.java
+++ Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/PremiumActivity.java
@@ -7,59 +7,71 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 import com.orm.query.Condition;
 import com.orm.query.Select;
 
-import java.util.Collections;
-import java.util.List;
-
 import uk.co.jakelee.blacksmith.R;
 import uk.co.jakelee.blacksmith.controls.TextViewPixel;
-import uk.co.jakelee.blacksmith.helper.IabUtils.IabHelper;
-import uk.co.jakelee.blacksmith.helper.IabUtils.IabResult;
-import uk.co.jakelee.blacksmith.helper.IabUtils.Purchase;
 import uk.co.jakelee.blacksmith.helper.ToastHelper;
 import uk.co.jakelee.blacksmith.model.Player_Info;
 import uk.co.jakelee.blacksmith.model.Upgrade;
 
-public class PremiumActivity extends Activity {
+public class PremiumActivity extends Activity implements BillingProcessor.IBillingHandler {
+    BillingProcessor bp;
+    boolean canBuyIAPs = false;
     private static final String SKU_PREMIUM = "premium";
-    private static final List<String> SKUs = Collections.singletonList(SKU_PREMIUM);
-    private static IabHelper ih;
-    private Activity premiumActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_premium);
-        premiumActivity = this;
 
+        canBuyIAPs = BillingProcessor.isIabServiceAvailable(this);
+        if (canBuyIAPs) {
+            bp = new BillingProcessor(this, getPublicKey(), this);
+        }
         updatePremiumStatus();
-
-        ih = new IabHelper(this, getPublicKey());
-        ih.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-            public void onIabSetupFinished(IabResult result) {
-                if (!result.isSuccess()) {
-                    Log.d("IAB", "Problem setting up In-app Billing: " + result);
-                }
-            }
-        });
     }
 
-    private IabHelper.OnIabPurchaseFinishedListener getPurchaseListener() {
-        return new IabHelper.OnIabPurchaseFinishedListener() {
-            public void onIabPurchaseFinished(IabResult result, Purchase purchase)
-            {
-                if (result.isFailure()) {
-                    Log.d("Blacksmith", "Error purchasing: " + result);
-                } else if (purchase.getSku().equals(SKU_PREMIUM)) {
-                    Log.d("Blacksmith", "Success purchasing: " + result);
-                    addPremiumFeatures();
-                    ToastHelper.showToast(premiumActivity, Toast.LENGTH_LONG, R.string.boughtPremium);
-                    updatePremiumStatus();
-                }
-            }
-        };
+    @Override
+    public void onBillingInitialized() {
+        if (bp.isPurchased(SKU_PREMIUM) && !Player_Info.isPremium()) {
+            addPremiumFeatures();
+            updatePremiumStatus();
+
+            ToastHelper.showToast(this, Toast.LENGTH_LONG, R.string.restoredPremium);
+            Log.d("Blacksmith", "IAP Engine initialised.");
+        }
+    }
+
+    @Override
+    public void onProductPurchased(String productId, TransactionDetails details) {
+        if (productId.equals(SKU_PREMIUM)) {
+            addPremiumFeatures();
+            updatePremiumStatus();
+
+            ToastHelper.showToast(this, Toast.LENGTH_LONG, R.string.boughtPremium);
+            Log.d("Blacksmith", "Purchased premium!");
+        }
+    }
+
+    @Override
+    public void onBillingError(int errorCode, Throwable error) {
+        ToastHelper.showToast(this, Toast.LENGTH_LONG, R.string.buyingPremiumError);
+        Log.d("Blacksmith", "Error occurred, code: " + errorCode);
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+        Log.d("IAB", "Purchases restored...");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data))
+            super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void updatePremiumStatus() {
@@ -78,24 +90,40 @@ public class PremiumActivity extends Activity {
     }
 
     public void buyPremium(View v) {
-        ih.launchPurchaseFlow(premiumActivity,
-                SKU_PREMIUM,
-                10001,
-                getPurchaseListener(),
-                "uniqueidentifyingstring");
+        if (canBuyIAPs) {
+            bp.purchase(this, SKU_PREMIUM);
+        } else {
+            ToastHelper.showToast(this, Toast.LENGTH_LONG, R.string.cannotBuyPremium);
+        }
     }
 
     @Override
     public void onDestroy() {
+        if (bp != null)
+            bp.release();
         super.onDestroy();
-        if (ih != null) {
-            ih.dispose();
-            ih = null;
-        }
     }
 
     private String getPublicKey() {
-        return "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlONTh/sroNhFQmK14kVk3H0bhmwJfcYpLdtZRSByLyuMlQO+yCY8EgjvLYhwXG+pSc8vZ5ib/y5DCu0aRVAsscYFcM9aAd8O5iTpwbgGqv5UyOMUNE5J3LDu9gSarDHdGOb/CsrfcPcW2WTHbiUCmkDevpFkV0sVInGZ5q0RMtDcOTcCgd5VLIeDa6VFo2Gv69VaHRQsjTu+4MEeGAa3FB47LIFUp/7jsHjB4vYt4bNH5CGTU9BL4pAwJXYif4dMa5r7fnCZQu0s5Kzis4dV919GEQ8BRMYZ+ZcyyCq8DiUjCc9exaszBqUxbCFROfeZ2NBDJPkRGHsgQU75yGolnwIDAQAB";
+        String[] keyArray = new String[] {
+                "MIIBIjANBgkqhki",
+                "G9w0BAQEFAAOCAQ",
+                "8AMIIBCgKCAQEAlONTh/sroNhFQ",
+                "mK14kVk3H0bhmwJfcYpLdtZRSByL",
+                "yuMlQO+yCY8EgjvLYhwXG+pSc8vZ5ib/y5DCu0aRVAsscYFcM9aAd8",
+                "O5iTpwbgGqv5UyOMUNE5J3LDu9gSarDHdGOb/CsrfcPc",
+                "W2WTHbiUCmkDevpFkV0sVInGZ5q0RMtDcOTcCgd5VLIeDa6VFo2Gv69Va",
+                "HRQsjTu+4MEeGAa3FB47LIFUp/7jsHjB4vYt4bNH5CGTU",
+                "9BL4pAwJXYif4dMa5r7fnCZQu0s5Kzis4dV919GEQ8BR",
+                "MYZ+ZcyyCq8DiUjCc9exaszBqUxbCFROfeZ2NBDJPkRGHsgQU75yGolnwIDAQAB"
+        };
+
+        StringBuilder builder = new StringBuilder();
+        for (String keyPart : keyArray) {
+            builder.append(keyPart);
+        }
+
+        return builder.toString();
     }
 
     public void openHelp(View view) {
@@ -121,18 +149,6 @@ public class PremiumActivity extends Activity {
                 Condition.prop("name").eq("XP Bonus")).first();
         xpBonus.setCurrent(xpBonus.getCurrent() + 20);
         xpBonus.setMaximum(xpBonus.getMaximum() + 50);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.i("IAB", "onActivityResult(" + requestCode + "," + resultCode + "," + data);
-
-        // Pass on the activity result to the helper for handling
-        if (!ih.handleActivityResult(requestCode, resultCode, data)) {
-            super.onActivityResult(requestCode, resultCode, data);
-        } else {
-            Log.i("IAB", "onActivityResult handled by IABUtil.");
-        }
     }
 
     public void closePopup(View view) {
