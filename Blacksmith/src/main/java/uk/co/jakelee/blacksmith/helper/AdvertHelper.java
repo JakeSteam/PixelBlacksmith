b5cb387094a96ba059bef2bd7cd6c9f54964c2b2
Jake <jake_l@live.co.uk>
2016-05-14 21:41:53 +0100

    Added reliable advert watching callback.

--- Blacksmith/src/main/java/uk/co/jakelee/blacksmith/helper/AdvertHelper.java
+++ Blacksmith/src/main/java/uk/co/jakelee/blacksmith/helper/AdvertHelper.java
@@ -8,12 +8,14 @@ import com.applovin.adview.AppLovinIncentivizedInterstitial;
 import com.applovin.sdk.AppLovinAd;
 import com.applovin.sdk.AppLovinAdDisplayListener;
 import com.applovin.sdk.AppLovinAdRewardListener;
+import com.applovin.sdk.AppLovinAdVideoPlaybackListener;
 import com.applovin.sdk.AppLovinSdk;
 
 import java.util.Map;
 
-public class AdvertHelper implements AppLovinAdRewardListener, AppLovinAdDisplayListener {
+public class AdvertHelper implements AppLovinAdRewardListener, AppLovinAdDisplayListener, AppLovinAdVideoPlaybackListener {
     private Context context;
+    private boolean verified;
     public AppLovinIncentivizedInterstitial myIncent;
 
     public AdvertHelper(Context context) {
@@ -25,33 +27,29 @@ public class AdvertHelper implements AppLovinAdRewardListener, AppLovinAdDisplay
     }
 
     public void showAdvert(Activity activity) {
-        myIncent.show(activity, this, null, this);
+        verified = false;
+        myIncent.show(activity, this, this, this);
     }
 
     @Override
     public void adHidden(AppLovinAd appLovinAd) {
+        if (verified) {
+            // Display reward message / dialog / whatever
+            ToastHelper.showPositiveToast(context, Toast.LENGTH_LONG, "Woo, you watched an ad!", false);
+        }
         myIncent.preload(null);
     }
 
     @Override
-    public void adDisplayed(AppLovinAd appLovinAd) {}
-
-    @Override
     public void userRewardVerified(AppLovinAd appLovinAd, Map map) {
-        ToastHelper.showPositiveToast(context, Toast.LENGTH_LONG, "Woo, you watched an ad!", false);
+        verified = true;
     }
 
-    @Override
-    public void userOverQuota(AppLovinAd appLovinAd, Map map) {
-        ToastHelper.showErrorToast(context, Toast.LENGTH_LONG, "Oh no, over quota!", false);
-    }
-
-    @Override
-    public void userRewardRejected(AppLovinAd appLovinAd, Map map) {}
-
-    @Override
-    public void validationRequestFailed(AppLovinAd appLovinAd, int i) {}
-
-    @Override
-    public void userDeclinedToViewAd(AppLovinAd appLovinAd) {}
+    @Override public void videoPlaybackBegan(AppLovinAd appLovinAd) {}
+    @Override public void videoPlaybackEnded(AppLovinAd appLovinAd, double v, boolean b) {}
+    @Override public void adDisplayed(AppLovinAd appLovinAd) {}
+    @Override public void userOverQuota(AppLovinAd appLovinAd, Map map) {}
+    @Override public void userRewardRejected(AppLovinAd appLovinAd, Map map) {}
+    @Override public void validationRequestFailed(AppLovinAd appLovinAd, int i) {}
+    @Override public void userDeclinedToViewAd(AppLovinAd appLovinAd) {}
 }
