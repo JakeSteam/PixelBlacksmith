5cd6d83dffe65073d279ffc5e7e5f1dbb1e3514e
Jake <jake_l@live.co.uk>
2016-05-27 19:58:47 +0100

    Fixed legendary tier position not being saved at finishing table.

--- Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/TableActivity.java
+++ Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/TableActivity.java
@@ -56,7 +56,7 @@ public class TableActivity extends Activity {
         dh = DisplayHelper.getInstance(getApplicationContext());
         gh = new GestureHelper(getApplicationContext());
         displayedTier = MainActivity.prefs.getInt("tableTier", booksSelected ? Constants.TIER_NONE : Constants.TIER_MIN);
-        if (displayedTier > Constants.TIER_MAX) displayedTier = Constants.TIER_MAX;
+        if (displayedTier > Constants.TIER_MAX && displayedTier != Constants.TIER_PREMIUM) displayedTier = Constants.TIER_PREMIUM;
         booksSelected = MainActivity.prefs.getBoolean("tableTab", false);
 
         CustomGestureDetector customGestureDetector = new CustomGestureDetector();
