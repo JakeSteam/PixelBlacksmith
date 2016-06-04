85d62c376a3c6bf46db3a051d2e69a809ca3eeba
Jake <jake_l@live.co.uk>
2016-06-05 00:18:31 +0100

    Large style layout changes. Basic fullscreen implemented.

--- Blacksmith/src/main/java/uk/co/jakelee/blacksmith/helper/DisplayHelper.java
+++ Blacksmith/src/main/java/uk/co/jakelee/blacksmith/helper/DisplayHelper.java
@@ -10,11 +10,13 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.SystemClock;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -249,6 +251,26 @@ public class DisplayHelper {
             } else {
                 overflowDisplay.setText("");
             }
+        }
+    }
+
+    public void toggleFullscreen(Activity activity) {
+        int ints = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if (Build.VERSION.SDK_INT >= 19) {
+            if (activity.getWindow().getDecorView().getSystemUiVisibility() == ints) {
+                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            } else {
+                activity.getWindow().getDecorView().setSystemUiVisibility(ints);
+            }
+        } else {
+            WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
+            attrs.flags ^= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            activity.getWindow().setAttributes(attrs);
         }
     }
 
