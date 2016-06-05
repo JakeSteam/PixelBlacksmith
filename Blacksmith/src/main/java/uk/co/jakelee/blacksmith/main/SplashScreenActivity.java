4b0eb05b4b709363673bc78e84b80a1a666207aa
Jake <jake_l@live.co.uk>
2016-06-05 14:01:53 +0100

    Added animated loading screen, but it won't show.

--- Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/SplashScreenActivity.java
+++ Blacksmith/src/main/java/uk/co/jakelee/blacksmith/main/SplashScreenActivity.java
@@ -9,6 +9,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Tried creating toast, and tried running on UI thread. Don't trigger until main activity is ready...
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
